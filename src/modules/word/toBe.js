add2d2551997cb9165bbb0785ba413a6af8c65dd
OlgaShtante <olga.shtante@gmail.com>
2019-07-24 00:39:29 +0300

    update js modules

--- src/modules/word/toBe.js
+++ src/modules/word/toBe.js
@@ -1,4 +1,19 @@
-const button = document.querySelector(".forms");
-button.addEventListener("click", playForms);
+const btnForms = document.querySelector(".forms");
+const btnUsage = document.querySelector(".usage");
+const btnTranslator = document.querySelector(".translator");
 
-function playForms() {}
+btnForms.addEventListener("click", playForms);
+btnUsage.addEventListener("click", playUsage);
+btnTranslator.addEventListener("click", playTranslator);
+
+function playForms() {
+  window.location = "toBeForms.html";
+}
+
+function playUsage() {
+  window.location = "#"; //toBeUsage.html
+}
+
+function playTranslator() {
+  window.location = "#"; //toBeTranslator.html
+}
